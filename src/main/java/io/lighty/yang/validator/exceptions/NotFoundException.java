363e6bd88d2778aebe48d079462e74beed878a2b
Filip Špes <filip.spes@pantheon.tech>
2021-02-24 12:46:20 +0100

    Fix all bugs for lighty-yang-validator reported by SonarCloud.
    Refractored Custom NotFoundException to be more readable.
    Removed redundant variables.
    Fixed indents.

--- src/main/java/io/lighty/yang/validator/exceptions/NotFoundException.java
+++ src/main/java/io/lighty/yang/validator/exceptions/NotFoundException.java
@@ -1,8 +1,8 @@
 package io.lighty.yang.validator.exceptions;
 
 /**
- * This exception is thrown when value of Optional<> variable is not present.
- * This excpetion is for handlig 3 types of cases when module is not found,
+ * This exception is thrown when value of Optional variable is not present.
+ * This exception is for handle 3 types of cases when module is not found,
  * revision of module is not found or when some node is missing.
  * Takes two parameters. First is customMessage. It should describe what is not found(module, revision or node)
  * Second parameter is name of this what is not found.
