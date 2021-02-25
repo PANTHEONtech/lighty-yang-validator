085f415baab163dda198b1d75c1a1048bae73dae
Filip Špes <filip.spes@pantheon.tech>
2021-02-25 17:07:57 +0100

    Fix all bugs for lighty-yang-validator reported by SonarCloud.
    -fixed errors reported by CheckStyle

--- src/main/java/io/lighty/yang/validator/exceptions/NotFoundException.java
+++ src/main/java/io/lighty/yang/validator/exceptions/NotFoundException.java
@@ -1,8 +1,8 @@
 package io.lighty.yang.validator.exceptions;
 
 /**
- * Indicating that expected element was not found by lyv, for example when expected
- * revision or DataSchemaNode was not found
+ * Indicating that expected element was not found by lyv.
+ * For example when expected revision or DataSchemaNode was not found.
  */
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String customMessage, String name) {
