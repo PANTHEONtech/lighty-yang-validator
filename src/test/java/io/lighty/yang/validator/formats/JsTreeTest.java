74350fd092d4beddf7ddf514cb083b1fd5305f47
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-05-18 12:57:23 +0200

    Improve testing for JsTree format
    Added test case for generating HTML file from multiple modules to
    identify the issue with multiple <body> tags. The test asserts that
    the file contains the expected modules, verifies that there is only
    one <body> tag, and checks if the output matches the expected HTML file.
    
    JIRA: LIGHTY-182
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- src/test/java/io/lighty/yang/validator/formats/JsTreeTest.java
+++ src/test/java/io/lighty/yang/validator/formats/JsTreeTest.java
@@ -9,9 +9,11 @@ package io.lighty.yang.validator.formats;
 
 import static io.lighty.yang.validator.Main.startLyv;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 import com.google.common.collect.ImmutableList;
 import io.lighty.yang.validator.FormatTest;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -68,6 +70,34 @@ public class JsTreeTest extends FormatTest {
     @Override
     public void runDeviationTest() throws Exception {
         runJsTreeTest("moduleDeviation.html");
+    }
+
+    @Test
+    public void testMultipleFiles() throws IOException {
+        final String module1 = Paths.get(yangPath + "/ietf-connection-oriented-oam@2019-04-16.yang").toString();
+        final String module2 = Paths.get(yangPath + "/ietf-routing@2018-03-13.yang").toString();
+        builder.setYangModules(ImmutableList.of(module1, module2));
+        final List<FormatPlugin> formats = new ArrayList<>();
+        formats.add(new JsTree());
+        formatter = new Format(formats);
+        builder.setFormat("jstree");
+        final var configuration = builder.build();
+        startLyv(configuration, formatter);
+        final Path outLog = Paths.get(outPath).resolve("out.log");
+        String fileCreated = Files.readString(outLog);
+        final String compareWith = Files.readString(outLog.resolveSibling("compare").resolve("multipleModules.html"));
+
+        //assert that file contains the first module
+        assertTrue(fileCreated.contains("<b> module name ietf-connection-oriented-oam@2019-04-16, "
+                + "namespace urn:ietf:params:xml:ns:yang:ietf-connection-oriented-oam, prefix co-oam </b>"));
+        //assert that file contains the second module
+        assertTrue(fileCreated.contains("<b> module name ietf-routing@2018-03-13, "
+                + "namespace urn:ietf:params:xml:ns:yang:ietf-routing, prefix rt </b>"));
+        //assert that there is only one <body> tag
+        final int bodyTagCount = fileCreated.split("<body>").length - 1;
+        assertEquals(1, bodyTagCount);
+        //assert that outputs match
+        assertEquals(fileCreated.replaceAll("\\s+", ""), compareWith.replaceAll("\\s+", ""));
     }
 
     private void runJsTreeTest(final String comapreWithFileName) throws Exception {
